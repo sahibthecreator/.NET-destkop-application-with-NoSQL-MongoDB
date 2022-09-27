7c66af6ddca825ee8c5094bfbe25a3b676ab6580
Sahib <sahibzulfigar4@gmail.com>
2022-09-27 13:47:30 +0200

    User and Incident class

--- NoSqlProject/Model/Incident.cs
+++ NoSqlProject/Model/Incident.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace Model
 {
-    internal class Incident
+    public class Incident
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("date")]
+        public DateTime Date { get; set; }
+        [BsonElement("subject")]
+        public string Subject { get; set; }
+
+        [BsonElement("type")]
+        public string Type { get; set; }
+
+        [BsonElement("reporter_id")]
+        public string Reporter { get; set; }
+
+        [BsonElement("deadline")]
+        public DateTime Deadline { get; set; }
+
+        [BsonElement("description")]
+        public string description { get; set; }
+
+        [BsonElement("I/T")]
+        public int IncidentTicket { get; set; }
+
+        [BsonElement("status")]
+        public int status { get; set; }
     }
 }
--- NoSqlProject/Model/User.cs
+++ NoSqlProject/Model/User.cs
@@ -14,17 +14,22 @@ namespace Model
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        [BsonElement("name")]
-        public string Name { get; set; }
-
-        [BsonElement("username")]
-        public string Username { get; set; }
+        [BsonElement("firstName")]
+        public string FirstName { get; set; }
+        [BsonElement("lastName")]
+        public string LastName { get; set; }
 
         [BsonElement("password")]
         public string Password { get; set; }
 
         [BsonElement("email")]
         public string Email { get; set; }
+
+        [BsonElement("location")]
+        public string Location { get; set; }
+
+        [BsonElement("phone")]
+        public string Phone { get; set; }
 
         [BsonElement("type")]
         public string Type { get; set; }
