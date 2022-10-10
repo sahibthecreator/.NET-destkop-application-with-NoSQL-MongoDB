f4d9c33e2ed37462c178b8e70a671bb9b5250101
Salah-023 <90417315+Salah-023@users.noreply.github.com>
2022-10-10 13:21:01 +0200

    Added layers

---
+++ DAL/Base.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace DAL
+{
+    public class Base
+    {
+        MongoClient dbClient;
+
+        public Base()
+        {
+            dbClient = new MongoClient("mongodb+srv://admin:admin@cluster.70ubhms.mongodb.net/test");
+        }
+    }
+}
