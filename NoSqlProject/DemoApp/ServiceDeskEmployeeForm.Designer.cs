68373d828fd78bc384c3196d00c766e613cfa223
Jun Feng <678234@student.inholland.nl>
2022-10-13 14:04:39 -0700

    SeriveDeskEmployeeForm updated

--- NoSqlProject/DemoApp/ServiceDeskEmployeeForm.Designer.cs
+++ NoSqlProject/DemoApp/ServiceDeskEmployeeForm.Designer.cs
@@ -29,52 +29,139 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPageDashboard = new System.Windows.Forms.TabPage();
+            this.tabPageIncidentManagement = new System.Windows.Forms.TabPage();
+            this.tabPageUserManagement = new System.Windows.Forms.TabPage();
+            this.textBoxFilterByEmail = new System.Windows.Forms.TextBox();
+            this.buttonAddNewUser = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tabPageUserManagement.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPageDashboard);
+            this.tabControl1.Controls.Add(this.tabPageIncidentManagement);
+            this.tabControl1.Controls.Add(this.tabPageUserManagement);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(800, 450);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // tabPageDashboard
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 424);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPageDashboard.Location = new System.Drawing.Point(4, 29);
+            this.tabPageDashboard.Name = "tabPageDashboard";
+            this.tabPageDashboard.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageDashboard.Size = new System.Drawing.Size(792, 417);
+            this.tabPageDashboard.TabIndex = 0;
+            this.tabPageDashboard.Text = "Dashboard";
+            this.tabPageDashboard.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // tabPageIncidentManagement
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 424);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPageIncidentManagement.Location = new System.Drawing.Point(4, 29);
+            this.tabPageIncidentManagement.Name = "tabPageIncidentManagement";
+            this.tabPageIncidentManagement.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageIncidentManagement.Size = new System.Drawing.Size(792, 417);
+            this.tabPageIncidentManagement.TabIndex = 1;
+            this.tabPageIncidentManagement.Text = "Incident Management";
+            this.tabPageIncidentManagement.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // tabPageUserManagement
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(792, 424);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabPageUserManagement.Controls.Add(this.textBoxFilterByEmail);
+            this.tabPageUserManagement.Controls.Add(this.buttonAddNewUser);
+            this.tabPageUserManagement.Controls.Add(this.listView1);
+            this.tabPageUserManagement.Controls.Add(this.label2);
+            this.tabPageUserManagement.Location = new System.Drawing.Point(4, 29);
+            this.tabPageUserManagement.Name = "tabPageUserManagement";
+            this.tabPageUserManagement.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageUserManagement.Size = new System.Drawing.Size(792, 417);
+            this.tabPageUserManagement.TabIndex = 2;
+            this.tabPageUserManagement.Text = "User Management";
+            this.tabPageUserManagement.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFilterByEmail
+            // 
+            this.textBoxFilterByEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFilterByEmail.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxFilterByEmail.Location = new System.Drawing.Point(20, 67);
+            this.textBoxFilterByEmail.Name = "textBoxFilterByEmail";
+            this.textBoxFilterByEmail.Size = new System.Drawing.Size(214, 24);
+            this.textBoxFilterByEmail.TabIndex = 7;
+            this.textBoxFilterByEmail.Text = "Filter by email";
+            // 
+            // buttonAddNewUser
+            // 
+            this.buttonAddNewUser.BackColor = System.Drawing.Color.LawnGreen;
+            this.buttonAddNewUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddNewUser.Location = new System.Drawing.Point(553, 64);
+            this.buttonAddNewUser.Name = "buttonAddNewUser";
+            this.buttonAddNewUser.Size = new System.Drawing.Size(109, 33);
+            this.buttonAddNewUser.TabIndex = 6;
+            this.buttonAddNewUser.Text = "+ ADD NEW USER";
+            this.buttonAddNewUser.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(20, 117);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(642, 283);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "ID";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Email";
+            this.columnHeader7.Width = 200;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "First Name";
+            this.columnHeader8.Width = 120;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Last Name";
+            this.columnHeader9.Width = 120;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "# tickets";
+            this.columnHeader10.Width = 80;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "User management";
             // 
             // ServiceDeskEmployeeForm
             // 
@@ -85,6 +172,8 @@
             this.Name = "ServiceDeskEmployeeForm";
             this.Text = "ServiceDeskEmployeeForm";
             this.tabControl1.ResumeLayout(false);
+            this.tabPageUserManagement.ResumeLayout(false);
+            this.tabPageUserManagement.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -92,8 +181,17 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPageDashboard;
+        private System.Windows.Forms.TabPage tabPageIncidentManagement;
+        private System.Windows.Forms.TabPage tabPageUserManagement;
+        private System.Windows.Forms.TextBox textBoxFilterByEmail;
+        private System.Windows.Forms.Button buttonAddNewUser;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.Label label2;
     }
 }
